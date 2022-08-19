c8c4cb187fb03457885c4d3dea91647a055d7894
brandon <b97wright@gmail.com>
2022-08-19 16:13:48 -0400

    Assignment 1 B finish
    just need to add finishing touches

--- src/Assignment1B.java
+++ src/Assignment1B.java
@@ -1,27 +1,41 @@
 import java.util.Scanner;
-
 public class Assignment1B {
     public static void main(String[] args)
     {
         Scanner myObj = new Scanner(System.in);
-        double height, width, res;
+        double labG, assignG, midExamG, finalExamG, finalG;
 
-        System.out.println("[Print Resolution]\n");
+        System.out.println("[CSE 1321L Final Grade Calculator]\n");
 
-        System.out.print("Enter the width (in pixels): ");
-        height = Double.parseDouble(myObj.nextLine());
+        System.out.print("Enter your average lab grade: ");
+        labG = Double.parseDouble(myObj.nextLine());
 
-        System.out.print("Enter the height (in pixels): ");
-        width = Double.parseDouble(myObj.nextLine());
+        System.out.print("Enter your average assignment grade: ");
+        assignG = Double.parseDouble(myObj.nextLine());
 
-        System.out.print("Enter the resolution (PPI): ");
-        res = Double.parseDouble(myObj.nextLine());
+        System.out.print("Enter your midterm exam grade: ");
+        midExamG = Double.parseDouble(myObj.nextLine());
 
-        width = width / res;
-        height = height / res;
+        System.out.print("Enter your final exam grade: ");
+        finalExamG = Double.parseDouble(myObj.nextLine());
+        System.out.println(" ");
 
-        System.out.println("At " + String.format("%.0f", res) + " PPI, the image is " + String.format("%.3f", height) + "\" wide by " + String.format("%.3f", width) + "\" high.");
+        labG = labG * .1;
+        assignG = assignG * .4;
+        midExamG = midExamG * .2;
+        finalExamG = finalExamG * .3;
 
+        System.out.println("Your weighted lab average is " + String.format("%.2f", labG));
+        System.out.println("Your weighted assignment average is " + String.format("%.2f", assignG));
+        System.out.println("Your weighted midterm exam average is " + String.format("%.2f", midExamG));
+        System.out.println("Your weighted final exam average is " + String.format("%.2f", finalExamG));
+        System.out.println(" ");
+
+        finalG = labG + assignG + midExamG + finalExamG;
+
+        System.out.println("Adding numbers...");
+        System.out.println("Your final grade in CSE 1321L is " + String.format("%.2f", finalG));
+    
     }
 }
 
