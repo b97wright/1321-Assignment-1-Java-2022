e32149b6095384aba2ee18af2d6e8d52149a1e87
brandon <b97wright@gmail.com>
2022-08-19 14:14:05 -0400

    Assignment 1 A Finished

--- src/Assignment1A.java
+++ src/Assignment1A.java
@@ -1,6 +1,26 @@
+import java.util.Scanner;
+
 public class Assignment1A {
     public static void main(String[] args)
     {
-        System.out.println("Hello This is a test!!");
+        Scanner myObj = new Scanner(System.in);
+        double height, width, res;
+
+        System.out.println("[Print Resolution]\n");
+
+        System.out.print("Enter the width (in pixels): ");
+        height = Double.parseDouble(myObj.nextLine());
+
+        System.out.print("Enter the height (in pixels): ");
+        width = Double.parseDouble(myObj.nextLine());
+
+        System.out.print("Enter the resolution (PPI): ");
+        res = Double.parseDouble(myObj.nextLine());
+
+        width = width / res;
+        height = height / res;
+
+        System.out.println("At " + String.format("%.0f", res) + " PPI, the image is " + String.format("%.3f", height) + "\" wide by " + String.format("%.3f", width) + "\" high.");
+
     }
 }
